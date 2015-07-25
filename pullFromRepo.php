efb160de53ca4960946cedea64da250f0110d6a2
Dr2n <darrenfu@outlook.com>
2015-07-25 17:39:51 +1000

    Create pullFromRepo.php

---
+++ pullFromRepo.php
@@ -0,0 +1,11 @@
+<?php
+	$output = `rm -rf *`;
+	echo "<br>".$output;
+	$output = `git clone -b Production https://github.com/The-Problem/The-Problem.git`;
+	echo "<br>".$output;
+	$output = `mv The-Problem/* .`;
+	echo "<br>".$output;
+	$output = `rm -rf The-Problem`;
+	echo "<br>".$output;
+	echo '<h2>Updated with GitHub Repository</h2>';
+?>
