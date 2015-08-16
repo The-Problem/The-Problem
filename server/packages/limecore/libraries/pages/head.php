292d778f13540be9a9806b7cdd8514a215c0e500
mrfishie <mrfishie101@hotmail.com>
2015-08-16 14:55:46 +1000

    local jquery and json3 Fetching from the CDN was slow (for development at least)

--- server/packages/limecore/libraries/pages/head.php
+++ server/packages/limecore/libraries/pages/head.php
@@ -49,13 +49,15 @@ class Head {
             //global $this;
 
             $code = '<title>' . $this->title . '</title>' . "\n";
-            
+
+            $resjs = Path::getclientfolder("res", "js", "lib");
+
             // If in IE <9, load jQuery 1.x and JSON2, else load jQuery 2.x
-            $code .= '<!--[if lt IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script><script src="//cdnjs.cloudflare.com/ajax/libs/json3/3.3.2/json3.min.js"></script><![endif]-->' . "\n";
-            $code .= '<!--[if gte IE 9]><!--><script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script><!--<![endif]-->' . "\n";
+            $code .= '<!--[if lt IE 9]><script src="' . $resjs . 'jquery-1.11.3.min.js"></script><script src="' . $resjs . 'json3.min.js"></script><![endif]-->' . "\n";
+            $code .= '<!--[if gte IE 9]><!--><script src="' . $resjs . 'jquery-2.1.4.min.js"></script><!--<![endif]-->' . "\n";
             
             // LimePHP JS API
-            $code .= '<script src="' . Path::getclientfolder("res", "js", "lib") . 'limephp.js"></script><script>LimePHP.SERVER["root"]="' . Path::getclientfolder() . '";$(function(){LimePHP.load()})</script>' . "\n";
+            $code .= '<script src="' . $resjs . 'limephp.js"></script><script>LimePHP.SERVER["root"]="' . Path::getclientfolder() . '";$(function(){LimePHP.load()})</script>' . "\n";
             
             // Default meta tags
             $code .= '<meta name="generator" content="LimePHP/' . LimePHP::VERSION . '" />' . "\n";
