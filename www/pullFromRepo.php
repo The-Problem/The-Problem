b7d0589aa57124bd987c9cf70e84a5ff62f0d2df
mrfishie <mrfishie101@hotmail.com>
2015-08-17 20:00:14 +1000

    update pull script

--- www/pullFromRepo.php
+++ www/pullFromRepo.php
@@ -32,6 +32,7 @@ run("git clone -b Development https://github.com/The-Problem/The-Problem.git");
 run("mv The-Problem/* .");
 run("rm -rf The-Problem");
 run("cp server/profile-production.php server/profile.php");
+run("cp ../database.php server/database.php");
 echo "</code></pre>";
 echo "<p>Success! Updated with Github repository.</p>";
 
