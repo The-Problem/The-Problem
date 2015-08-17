93155a6cb663766cf842fbee42baf8d05aaacfc7
mrfishie <mrfishie101@hotmail.com>
2015-08-17 18:36:51 +1000

    initial admin page

--- server/packages/problem/pages/admin/admin.php
+++ server/packages/problem/pages/admin/admin.php
@@ -1,6 +1,16 @@
 <?php
 class AdminPage implements IPage {
+    private $pages = array(
+        "overview" => "Overview",
+        "sections" => "Sections",
+        "permissions" => "Permissions"
+    );
+
+    private $page = "overview";
+
     public function __construct(PageInfo &$page) {
+        $path = $page->pagelist;
+        if (count($path) > 1) $this->page = $path[1];
     }
     public function template() {
         return Templates::findtemplate("default");
@@ -23,17 +33,69 @@ class AdminPage implements IPage {
 
     public function head(Head &$head) {
         $head->title .= " - Admin";
+        $head->stylesheet("pages/admin");
+    }
+
+    public function overview() {
+        ?>
+        <section>
+            <h2>Site Name</h2>
+            <input type="text" placeholder="The Problem" name="sitename" />
+            <p class="help">The site name is displayed on the homepage, title bar, and various places throughout the
+            site.</p>
+            <p class="tip">Set the site name to the product or company that the site is being used for.</p>
+        </section><section class="site-visibility">
+            <h2>Site Visibility</h2>
+            <div class="columns">
+                <div class="column">
+                    <label><input type="radio" name="visibility" value="public" />Public</label><br>
+                    <label><input type="radio" name="visibility" value="private" />Private</label>
+                </div>
+                <div class="column registration">
+                    <label><input type="radio" name="registration" value="open" />Open registration</label><br>
+                    <label><input type="radio" name="registration" value="closed" />Closed registration</label>
+                </div>
+            </div>
+            <p class="help">Site visibility allows you to control who can view your website. By setting the visibility
+            to private, registrations will require confirmation from an administrator or moderator, and sections will
+            only be viewable by registered users. Public allows anyone to view or register for the site.</p>
+            <p class="tip">If the site is being used as an internal bug tracker but will be exposed to the Internet,
+            set this option to Private.</p>
+        </section>
+        <?php
+    }
+    public function sections() {
+
+    }
+    public function permissions() {
+
     }
 
     public function body() {
         ?>
 <h1>Admin Control Panel</h1>
 <div class="container">
-    <div class="panel"></div>
-    <nav>
-        <a class="item" href="<?php echo Path::getclientfolder('admin', 'overview'); ?>" title="Overview">Overview</a>
-        <a class="item" href="<?php echo Path::getclientfolder('admin', 'sections'); ?>" title="Sections">Sections</a>
-        <a class="item" href="<?php echo Path::getclientfolder('admin', 'permissions'); ?>" title="Permissions">Permissions</a>
+    <div class="panel">
+        <?php
+        switch ($this->page) {
+            case 'sections': $this->sections(); break;
+            case 'permissions': $this->permissions(); break;
+            default: $this->overview(); break;
+        }
+        ?>
+    </div><nav>
+        <?php
+        end($this->pages);
+        $last = key($this->pages);
+
+        foreach ($this->pages as $path => $name) {
+            ?>
+        <a class="item<?php if ($path === $last) echo ' last';
+                            if ($path === $this->page) echo ' selected'; ?>" href="<?php echo Path::getclientfolder('admin', $path);
+        ?>" title="<?php echo $name; ?>"><?php echo $name; ?></a>
+            <?php
+        }
+        ?>
         <a class="return" href="<?php echo Path::getclientfolder(); ?>" title="Back to site">Back to site</a>
     </nav>
 </div>
