5230ebfe9c5da953ed0d34843fb6b36b7c87be6d
mrfishie <mrfishie101@hotmail.com>
2015-08-18 21:40:15 +1000

    import SQL in pull script

--- www/pullFromRepo.php
+++ www/pullFromRepo.php
@@ -24,7 +24,7 @@
 echo "<p>Starting pull...</p>";
 echo "<pre><code>";
 
-echo "<span>$</span> cd ../\n";
+echo "<span>$</span> cd ../\n\n";
 chdir(__DIR__ . '/../');
 
 run("rm -rf *");
@@ -33,6 +33,18 @@ run("mv The-Problem/* .");
 run("rm -rf The-Problem");
 run("cp server/profile-production.php server/profile.php");
 run("cp ../database.php server/database.php");
+
+echo "<span>$</span>cat database.sql | ./import_sql\n";
+
+require('../server/database.php');
+$con = new MySQLi(LIME_DB_HOST, LIME_DB_USERNAME, LIME_DB_PASSWORD, LIME_DB_DATABASE);
+$con->multi_query(file_get_contents("database.sql"));
+echo "Checking for presence of tables...\n";
+$res = $con->query("SHOW TABLES LIKE 'users'");
+if ($res->num_rows <= 0) echo "<span style='color:red'>Warning!</span> It doesn't look like the tables imported correctly.\n";
+else echo "Everything looks OK.\n";
+echo "Finished importing tables.\n\n";
+
 echo "</code></pre>";
 echo "<p>Success! Updated with Github repository.</p>";
 
