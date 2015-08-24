9992c7ba179fd16eec5a19886fd7179ef79d91b2
mrfishie <mrfishie101@hotmail.com>
2015-08-24 21:46:45 +1000

    permission system works with sections

--- server/packages/problem/libraries/objects/objects.php
+++ server/packages/problem/libraries/objects/objects.php
@@ -5,22 +5,113 @@ class Objects {
     const TYPE_BUG = 1;
     const TYPE_COMMENT = 2;
 
-    private static $cache = array();
+    private static $permissions = array();
 
-    public static function query($query, $types = false, $args = false) {
-        Library::get("connection");
-        $items = Connection::query($query, $types, $args);
-        if ($items) return self::to_object_list($items);
-        return array();
+    public static function permission($object_id, $type, $username) {
+        $keyname = "0$object_id.$username.$type";
+        if (array_key_exists($keyname, self::$permissions)) return self::$permissions[$keyname];
+
+        $results = Connection::query("
+SELECT COUNT(*) AS Has_Permission,
+  userpermissions.Username,
+  users.Username,
+  grouppermissions.Rank, GREATEST(
+              CASE WHEN developers.Username IS NULL
+                THEN 0
+              ELSE 2
+              END,
+              COALESCE(users.Rank, 0)
+          ) AS User_rank
+FROM objects
+  LEFT JOIN users ON (users.Username = ?)
+  JOIN sections ON (sections.Object_ID = objects.Object_ID)
+  JOIN developers ON (developers.Section_ID = sections.Section_ID AND developers.Username = users.Username)
+
+  LEFT JOIN userpermissions ON (userpermissions.Username = users.Username
+                                AND userpermissions.Permission_Name = ?
+                                AND userpermissions.Object_ID = objects.Object_ID)
+  LEFT JOIN grouppermissions ON (grouppermissions.Permission_Name = ?
+                                 AND grouppermissions.Object_ID = objects.Object_ID)
+WHERE objects.Object_ID = ? AND (? IS NULL OR users.Username = ?)
+HAVING userpermissions.Username = users.Username
+  OR User_rank >= grouppermissions.Rank", "isssss", array($object_id, $username, $type, $type, $username, $username));
+
+        $value = $results[0]["Has_Permission"] > 0;
+        self::$permissions[$keyname] = $value;
+        return $value;
     }
 
-    public static function to_object_list($items) {
-        return array_map(function($item) {
-            if (array_key_exists($item['Object_ID'], self::$cache)) return self::$cache[$item['Object_ID']];
+    public static function user_permissions($type, $username) {
+        $keyname = "1$username.$type";
+        if (array_key_exists($keyname, self::$permissions) && self::$permissions[$keyname] !== FALSE) return self::$permissions[$keyname];
 
-            $new_obj = new Object($item);
-            self::$cache[$item['Object_ID']] = $new_obj;
-            return $new_obj;
-        }, $items);
+        $results = Connection::query("
+SELECT userpermissions.Username,
+  users.Username,
+  userpermissions.Object_ID AS Object_ID1,
+  grouppermissions.Object_ID AS Object_ID2,
+  grouppermissions.Rank,
+  GREATEST(
+      CASE WHEN developers.Username IS NULL
+        THEN 0
+      ELSE 2
+      END,
+      COALESCE(users.Rank, 0)
+  ) AS User_rank
+FROM objects
+  LEFT JOIN users ON (users.Username = 'mrfishie')
+  LEFT JOIN sections ON (objects.Object_Type = 0 AND sections.Object_ID = objects.Object_ID)
+  LEFt JOIN developers ON (developers.Section_ID = sections.Section_ID AND developers.Username = users.Username)
+
+  LEFT JOIN userpermissions ON (userpermissions.Permission_Name = 'user.view'
+                                AND userpermissions.Object_ID = objects.Object_ID)
+  LEFT JOIN grouppermissions ON (grouppermissions.Permission_Name = 'user.view'
+                                 AND grouppermissions.Object_ID = objects.Object_ID)
+WHERE ('mrfishie' IS NULL OR users.Username = 'mrfishie')
+HAVING userpermissions.Username = users.Username
+       OR User_rank >= grouppermissions.Rank", "sssss", array($username, $username, $username));
+
+        $value = array();
+        foreach ($results as $item) {
+            if (!is_null($item["Object_ID1"])) {
+                $value[$item["Object_ID1"]] = true;
+                self::$permissions["0" . $item["Object_ID1"] . ".$username.$type"] = true;
+            }
+            if (!is_null($item["Object_ID2"])) {
+                $value[$item["Object_ID2"]] = true;
+                self::$permissions["0" . $item["Object_ID2"] . ".$username.$type"] = true;
+            }
+        }
+
+        self::$permissions[$keyname] = $value;
+        return $value;
+    }
+
+    public static function allow_user($object_id, $type, $username) {
+        Connection::query("INSERT INTO userpermissions (Object_ID, Permission_Name, Username) VALUES (?, ?, ?)",
+            "iss", array($object_id, $type, $username));
+
+        self::$permissions["0$object_id.$username.$type"] = true;
+        self::$permissions["1$username.$type"][$object_id] = true;
+    }
+    public static function deny_user($object_id, $type, $username) {
+        Connection::query("DELETE FROM userpermissions WHERE Object_ID = ?
+                                                       AND Permission_Name = ?
+                                                       AND Username = ?", "iss",
+            array($object_id, $type, $username));
+
+        self::$permissions["0$object_id.$username.$type"] = false;
+        self::$permissions["1$username.$type"][$object_id] = false;
+    }
+
+    public static function allow_rank($object_id, $type, $rank) {
+        Connection::query("INSERT INTO grouppermissions (Object_ID, Permission_Name, Rank) VALUES (?, ?, ?)",
+            "isi", array($object_id, $type, $rank));
+    }
+    public static function deny_rank($object_id, $type, $rank) {
+        Connection::query("DELETE FROM grouppermissions WHERE Object_ID = ?
+                                                        AND Permission_Name = ?
+                                                        AND Rank = ?", "isi",
+            array($object_id, $type, $rank));
     }
 }
