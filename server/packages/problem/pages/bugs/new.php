2b39209e173306ce0230909796cdd5b0837880b9
Tom Barham <mrfishie101@hotmail.com>
2015-08-31 10:13:00 +1000

    go back

--- server/packages/problem/pages/bugs/new.php
+++ server/packages/problem/pages/bugs/new.php
@@ -25,102 +25,56 @@ class BugsNewPage implements IPage {
 
     public function head(Head &$head) {
         $head->stylesheet("pages/new");
-
-        if (array_key_exists("name", $_POST)) {
+         if (array_key_exists("name", $_POST)) {
             // create the object
             Connection::query("INSERT INTO objects (Object_Type) VALUES (1)");
             $object_id = Connection::insertid();
-
-            // PROCESS BUG CONTENTS
-            $value = $_POST['description'];
-
-            // find @mentions
-            $mentions = array();
-            preg_match_all("/@([^@ ]+)/", $value, $mentions);
-
-            $items = array_unique($mentions[1]);
-            foreach ($items as $name) {
-                $user = Connection::query("SELECT Username FROM users WHERE Username = ?", "s", array($name));
-                if (count($user)) {
-                    Connection::query("INSERT INTO notifications
-                                     (Triggered_By, Received_By, Target_One, Target_Two, Creation_Date, Type)
-                              VALUES (           ?,           ?,          ?,          ?,             ?,    ?)", "ssiisi", array(
-                        $_SESSION["username"], $name, $this->section_obj, $object_id, date('Y-m-d H:i:s'), 3
-                    ));
-
-                    $value = str_replace("@$name", "[@$name](" . Path::getclientfolder("~$name") . ")", $value);
-                }
-            }
-
-            // find #bugs
-            $bug_refs = array();
-            preg_match_all("/([\\w-]*)#(\\d+)/", $value, $bug_refs);
-
-            $section_names = $bug_refs[1];
-            $bug_ids = $bug_refs[2];
-
-            $bugs = array();
-            foreach ($bug_ids as $k => $id) {
-                $section_name = $section_names[$k];
-                if (!strlen($section_name)) {
-                    $section_name = $this->section;
-                    array_push($bugs, array(strtolower($section_name), $id, "#$id"));
-                }
-
-                array_push($bugs, array(strtolower($section_name), $id, "$section_name#$id"));
-            }
-            $unique_bugs = array_unique($bugs);
-            foreach ($unique_bugs as $b) {
-                $value = str_replace($b[2], "[" . $b[2] . "](" . Path::getclientfolder("bugs", $b[0], $b[1])  . ")", $value);
-            }
-
-            Library::get('parsedown');
-            $parsedown = new Parsedown();
-            $value = $parsedown->text($value);
-
+            // Parse contents of the post
+            Library::get("parser");
+            $value = Parser::parse($_POST['description'], $_SESSION["username"], array(
+                "parent_object_id" => $object_id,
+                "current_object_id" => $object_id,
+                "section_slug" => $this->section
+            ));
+            // calculate the new relative ID of the bug
             $rid = Connection::query("SELECT COUNT(*) + 1 AS New_RID FROM bugs WHERE bugs.Section_ID = ?", "i", array($this->section_id));
-
             // create the bug object
-            Connection::query("INSERT INTO bugs (Section_ID, Object_ID, Name, Status, Description, Creation_Date, Author, RID)
-                                         VALUES (         ?,         ?,    ?,      1,           ?,             ?,      ?, ?)",
-                "iissssi", array(
-                    $this->section_id, $object_id, $_POST['name'], $value, date("Y-m-d H:i:s"), $_SESSION["username"], $rid[0]["New_RID"]
-                ));
-
+            Connection::query("INSERT INTO bugs (Section_ID, Object_ID, Name, Status, Description, Raw_Description, Creation_Date, Author, RID)
+            VALUES ( ?, ?, ?, 1, ?, ?, ?, ?, ?)",
+            "iisssssi", array(
+                $this->section_id, $object_id, $_POST['name'], $value, $_POST['description'], date("Y-m-d H:i:s"), $_SESSION["username"], $rid[0]["New_RID"]
+            ));
             // make the author of the bug follow the bug
             Connection::query("INSERT INTO watchers (Object_ID, Username) VALUES (?, ?)", "is", array($object_id, $_SESSION["username"]));
-
             // add a notification to followers of the section
             Connection::query("INSERT INTO notifications
-                                 (Triggered_By, Received_By,                    Target_One, Target_Two, Creation_Date, Type)
-                          VALUES (           ?, (SELECT Username
-                                                   FROM watchers
-                                                 WHERE watchers.Object_ID = ?),          ?,          ?,             ?,    1)",
-                "siiis", array($_SESSION["username"], $this->section_obj, $object_id, $this->section_obj, date('Y-m-d H:i:s')));
-
+            (Triggered_By, Received_By, Target_One, Target_Two, Creation_Date, Type)
+            VALUES ( ?, (SELECT Username
+            FROM watchers
+            WHERE watchers.Object_ID = ?), ?, ?, ?, 1)",
+            "siiis", array($_SESSION["username"], $this->section_obj, $object_id, $this->section_obj, date('Y-m-d H:i:s')));
             // get default permission values
             $default_view = Connection::query("SELECT Value FROM configuration
-                                             WHERE Type = 'permissions-default-bugs'
-                                             AND Name = 'view'");
+                WHERE Type = 'permissions-default-bugs'
+                AND Name = 'view'");
             $default_status = Connection::query("SELECT Value FROM configuration
-                                               WHERE Type = 'permissions-default-bugs'
-                                               AND Name = 'status'");
+                WHERE Type = 'permissions-default-bugs'
+                AND Name = 'status'");
             $default_edit = Connection::query("SELECT Value FROM configuration
-                                               WHERE Type = 'permissions-default-bugs'
-                                               AND Name = 'edit'");
+                WHERE Type = 'permissions-default-bugs'
+                AND Name = 'edit'");
             $default_delete = Connection::query("SELECT Value FROM configuration
-                                               WHERE Type = 'permissions-default-bugs'
-                                               AND Name = 'delete'");
+                WHERE Type = 'permissions-default-bugs'
+                AND Name = 'delete'");
             $default_assign = Connection::query("SELECT Value FROM configuration
-                                               WHERE Type = 'permissions-default-bugs'
-                                               AND Name = 'assigning'");
+                WHERE Type = 'permissions-default-bugs'
+                AND Name = 'assigning'");
             $default_comment = Connection::query("SELECT Value FROM configuration
-                                               WHERE Type = 'permissions-default-comments'
-                                               AND Name = 'create'");
+                WHERE Type = 'permissions-default-comments'
+                AND Name = 'create'");
             $default_upvote = Connection::query("SELECT Value FROM configuration
-                                               WHERE Type = 'permissions-default-comments'
-                                               AND Name = 'upvote'");
-
+                WHERE Type = 'permissions-default-comments'
+                AND Name = 'upvote'");
             // set permission defaults for ranks
             Objects::allow_rank($object_id, "bug.view", $default_view[0]["Value"]);
             Objects::allow_rank($object_id, "bug.change-status", $default_status[0]["Value"]);
@@ -129,7 +83,6 @@ class BugsNewPage implements IPage {
             Objects::allow_rank($object_id, "bug.assign", $default_assign[0]["Value"]);
             Objects::allow_rank($object_id, "bug.comment", $default_comment[0]["Value"]);
             Objects::allow_rank($object_id, "comment.upvote", $default_upvote[0]["Value"]);
-
             // set permissions for the author of the bug
             Objects::allow_user($object_id, "bug.view", $_SESSION['username']);
             Objects::allow_user($object_id, "bug.change-status", $_SESSION["username"]);
@@ -137,12 +90,9 @@ class BugsNewPage implements IPage {
             Objects::allow_user($object_id, "comment.assign", $_SESSION["username"]);
             Objects::allow_user($object_id, "bug.comment", $_SESSION["username"]);
             Objects::allow_user($object_id, "comment.upvote", $_SESSION["username"]);
-
             // redirect to the bug page
             Path::redirect(Path::getclientfolder("bugs", $this->section, $rid[0]["New_RID"]));
         }
-
-
         $head->script("lib/autosize.min");
     }
 
